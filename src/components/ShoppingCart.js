a618ac7e1407697d95676bf1a43be8f9e344de39
Michael Williams <manicdesigner@gmail.com>
2019-10-16 18:03:26 -0400

    add CardContext and reformat file

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { CartContext } from '../context/contexts';
 
 // Components
 import Item from './ShoppingCartItem';
 
 const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+  const getCartTotal = () => {
+    return props.cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+  return (
+    <div className='shopping-cart'>
+      {props.cart.map(item => (
+        <Item key={item.id} {...item} />
+      ))}
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+      <div className='shopping-cart__checkout'>
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
